d7e174774729c1068d9df65b8c3977a675ccf19e
Alexander Dause <alexander.dause@web.de>
2024-10-21 21:08:04 +0200

    add environment for firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolBarComponent } from "./tool-bar/tool-bar.component";
+import { BrowserModule } from '@angular/platform-browser';
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { environment } from "../environments/environment"; 
+import { Firestore } from '@angular/fire/firestore';
 
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, ToolBarComponent],
+    imports: [RouterOutlet, ToolBarComponent, BrowserModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
     title = 'simpleCrm';
+    
 
-    firebaseConfig = {
-        apiKey: "AIzaSyBIA3rWRnvyvGM2VI6AgOnbtgEUtOhGsCY",
-        authDomain: "simple-crm-6733d.firebaseapp.com",
-        projectId: "simple-crm-6733d",
-        storageBucket: "simple-crm-6733d.appspot.com",
-        messagingSenderId: "1023126924437",
-        appId: "1:1023126924437:web:8f937be0291fb44fac7f9c"
-    };
-
-    app = initializeApp(this.firebaseConfig);
-    db = getFirestore(this.app);
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { provideFirestore } from '@angular/fire/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
